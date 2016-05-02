1034adac6c12fe4ce3fd05fa2b99b2d2a93fc375
emeric <itmfr@yahoo.fr>
2016-05-02 20:13:39 +0200

    [CONF] Resurrected config files

---
+++ src/config/Config.cpp
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2016 Emeric Poupon
+ *
+ * This file is part of LMS.
+ *
+ * LMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LMS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <sstream>
+
+#include "Config.hpp"
+
+namespace {
+
+}
+
+Config::Config()
+: _config (nullptr)
+{
+}
+
+Config&
+Config::instance()
+{
+	static Config instance;
+	return instance;
+}
+
+void
+Config::setFile(boost::filesystem::path p)
+{
+	if (_config != nullptr)
+		delete _config;
+
+	_config = new libconfig::Config();
+
+	_config->readFile(p.string().c_str());
+}
+
+std::string
+Config::getString(std::string setting, std::string def)
+{
+	try {
+		return _config->lookup(setting);
+	}
+	catch (std::exception &e)
+	{
+		return def;
+	}
+}
+
+unsigned long
+Config::getULong(std::string setting, unsigned long def)
+{
+	try {
+		return static_cast<unsigned int>(_config->lookup(setting));
+	}
+	catch (...)
+	{
+		return def;
+	}
+}
+
+long
+Config::getLong(std::string setting, long def)
+{
+	try {
+		return _config->lookup(setting);
+	}
+	catch (...)
+	{
+		return def;
+	}
+}
+
+bool
+Config::getBool(std::string setting, bool def)
+{
+	try {
+		return _config->lookup(setting);
+	}
+	catch (...)
+	{
+		return def;
+	}
+}
+
+
