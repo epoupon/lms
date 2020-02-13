934dd5adf13f155567eea4bc0eb9e3fa8aace6f0
emeric <itmfr@yahoo.fr>
2020-02-13 13:29:57 +0100

    Fixed regression about multi words parsing. Fixes #30

--- src/utils/String.hpp
+++ src/utils/String.hpp
@@ -57,6 +57,10 @@ std::optional<T> readAs(const std::string& str)
 	return res;
 }
 
+template<>
+std::optional<std::string>
+readAs(const std::string& str);
+
 std::string
 replaceInString(const std::string& str, const std::string& from, const std::string& to);
 
