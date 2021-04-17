epoupon/lms

commit eea27b86f0de49860e78ee8bd55d734640f33dfc

Switched from sync transcoder to async transcoder

src/libs/av/impl/TranscodeResourceHandler.cpp
@@ -17,7 +17,7 @@
  * along with LMS.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "AvTranscodeResourceHandler.hpp"
+#include "TranscodeResourceHandler.hpp"
 
 namespace Av
 {
@@ -36,24 +36,32 @@ namespace Av
 		_transcoder.start();
 	}
 
-	void
+	Wt::Http::ResponseContinuation*
 	TranscodeResourceHandler::processRequest(const Wt::Http::Request& /*request*/, Wt::Http::Response& response)
 	{
 		response.setMimeType(_transcoder.getOutputMimeType());
 
-		if (!_transcoder.isComplete())
+		if (_nbBytesReady > 0)
 		{
-			std::vector<unsigned char> buffer;
-
-			_transcoder.process(buffer, _chunkSize);
-			response.out().write(reinterpret_cast<const char *>(&buffer[0]), buffer.size());
+			response.out().write(reinterpret_cast<const char *>(&_buffer[0]), _nbBytesReady);
+			_nbBytesReady = 0;
 		}
-	}
 
-	bool
-	TranscodeResourceHandler::isFinished() const
-	{
-		return _transcoder.isComplete();
+		if (!_transcoder.finished())
+		{
+			Wt::Http::ResponseContinuation *continuation {response.createContinuation()};
+			continuation->waitForMoreData();
+			_transcoder.asyncRead(_buffer.data(), _buffer.size(), [=](std::size_t nbBytesRead)
+			{
+				assert(_nbBytesReady == 0);
+				_nbBytesReady = nbBytesRead;
+				continuation->haveMoreData();
+			});
+
+			return continuation;
+		}
+
+		return {};
 	}
 }
 

src/libs/av/impl/Transcoder.cpp
@@ -17,12 +17,12 @@
  * along with LMS.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "av/AvTranscoder.hpp"
+#include "Transcoder.hpp"
 
 #include <atomic>
-#include <mutex>
+#include <iomanip>
 
-#include "av/AvInfo.hpp"
+#include "utils/IChildProcessManager.hpp"
 #include "utils/IConfig.hpp"
 #include "utils/Path.hpp"
 #include "utils/Logger.hpp"
@@ -32,7 +32,7 @@ namespace Av {
 
 #define LOG(sev)	LMS_LOG(TRANSCODE, sev) << "[" << _id << "] - "
 
-static std::atomic<size_t>	globalId {};
+static std::atomic<size_t>		globalId {};
 static std::filesystem::path	ffmpegPath;
 
 void
@@ -40,39 +40,41 @@ Transcoder::init()
 {
 	ffmpegPath = Service<IConfig>::get()->getPath("ffmpeg-file", "/usr/bin/ffmpeg");
 	if (!std::filesystem::exists(ffmpegPath))
-		throw LmsException {"File '" + ffmpegPath.string() + "' does not exist!"};
+		throw Exception {"File '" + ffmpegPath.string() + "' does not exist!"};
 }
 
 Transcoder::Transcoder(const std::filesystem::path& filePath, const TranscodeParameters& parameters)
-: _filePath {filePath},
-  _parameters {parameters},
-  _id {globalId++}
+:  _id {globalId++}
+, _filePath {filePath}
+, _parameters {parameters}
 {
 
 }
+
+Transcoder::~Transcoder() = default;
 
 bool
 Transcoder::start()
 {
+	if (ffmpegPath.empty())
+		init();
+
 	try
 	{
 		if (!std::filesystem::exists(_filePath))
 		{
 			LOG(ERROR) << "File '" << _filePath << "' does not exist!";
-			_isComplete = true;
 			return false;
 		}
 		else if (!std::filesystem::is_regular_file( _filePath) )
 		{
 			LOG(ERROR) << "File '" << _filePath << "' is not regular!";
-			_isComplete = true;
 			return false;
 		}
 	}
 	catch (const std::filesystem::filesystem_error& e)
 	{
 		LOG(ERROR) << "File error on '" << _filePath.string() << "': " << e.what();
-		_isComplete = true;
 		return false;
 	}
 
@@ -82,17 +84,21 @@ Transcoder::start()
 
 	args.emplace_back(ffmpegPath.string());
 
-	// Make sure we do not produce anything in the stderr output
+	// Make sure:
+	// - we do not produce anything in the stderr output
+	// - we do not rely on input
 	// in order not to block the whole forked process
 	args.emplace_back("-loglevel");
 	args.emplace_back("quiet");
 	args.emplace_back("-nostdin");
 
 	// input Offset
-	if (_parameters.offset)
 	{
 		args.emplace_back("-ss");
-		args.emplace_back(std::to_string((*_parameters.offset).count()));
+
+		std::ostringstream oss;
+		oss << std::fixed << std::showpoint << std::setprecision(3) << (_parameters.offset.count() / float {1000});
+		args.emplace_back(oss.str());
 	}
 
 	// Input file
@@ -119,6 +125,7 @@ Transcoder::start()
 	// Output bitrates
 	args.emplace_back("-b:a");
 	args.emplace_back(std::to_string(_parameters.bitrate));
+
 
 	// Codecs and formats
 	switch (_parameters.format)
@@ -157,7 +164,6 @@ Transcoder::start()
 			break;
 
 		default:
-			_isComplete = true;
 			return false;
 	}
 
@@ -169,84 +175,56 @@ Transcoder::start()
 	for (const std::string& arg : args)
 		LOG(DEBUG) << "Arg = '" << arg << "'";
 
-	// make sure only one thread is executing this part of code
+	// Caution: stdin must have been closed before
+	try
 	{
-		static std::mutex transcoderMutex;
-
-		std::lock_guard<std::mutex> lock {transcoderMutex};
-
-		_child = std::make_shared<redi::ipstream>();
-
-		// Caution: stdin must have been closed before
-		_child->open(ffmpegPath.string(), args);
-		if (!_child->is_open())
-		{
-			LOG(DEBUG) << "Exec failed!";
-			_isComplete = true;
-			return false;
-		}
-
-		if (_child->out().eof())
-		{
-			LOG(DEBUG) << "Early end of file!";
-			_isComplete = true;
-			return false;
-		}
-
+		_childProcess = Service<IChildProcessManager>::get()->spawnChildProcess(ffmpegPath, args);
 	}
-	LOG(DEBUG) << "Stream opened!";
+	catch (ChildProcessException& exception)
+	{
+		LOG(ERROR) << "Cannot execute '" << ffmpegPath << "': " << exception.what();
+		return false;
+	}
 
 	return true;
 }
 
 void
-Transcoder::process(std::vector<unsigned char>& output, std::size_t maxSize)
+Transcoder::asyncWaitForData(WaitCallback cb)
 {
-	if (!_child || _isComplete)
-		return;
+	assert(_childProcess);
 
-	output.resize(maxSize);
+	LOG(DEBUG) << "Want to wait for data";
 
-	//Read on the output stream
-	_child->out().read(reinterpret_cast<char*>(&output[0]), maxSize);
-	output.resize(_child->out().gcount());
-
-	if (_child->out().fail())
+	_childProcess->asyncWaitForData([cb = std::move(cb)]
 	{
-		LOG(DEBUG) << "Stdout FAILED";
-		_isComplete = true;
-	}
-
-	if (_child->out().eof())
-	{
-		LOG(DEBUG) << "Stdout EOF!";
-		_isComplete = true;
-	}
-
-	if (_isComplete)
-	{
-		LOG(DEBUG) << "Transcode complete!";
-		_child->clear();
-		_child.reset();
-	}
-
-	_total += output.size();
-
-	LOG(DEBUG) << "nb bytes = " << output.size() << ", total = " << _total;
+		cb();
+	});
 }
 
-Transcoder::~Transcoder()
+void
+Transcoder::asyncRead(std::byte* buffer, std::size_t bufferSize, ReadCallback readCallback)
 {
-	LOG(DEBUG) << ", ~Transcoder called! Total produced bytes = " << _total;
+	assert(_childProcess);
 
-	if (_child)
+	return _childProcess->asyncRead(buffer, bufferSize, [readCallback {std::move(readCallback)}](IChildProcess::ReadResult /*res*/, std::size_t nbBytesRead)
 	{
-		LOG(DEBUG) << "Child still here!";
-		_child->rdbuf()->kill(SIGKILL);
-		LOG(DEBUG) << "Closing...";
-		_child->rdbuf()->close();
-		LOG(DEBUG) << "Closing DONE";
-	}
+		readCallback(nbBytesRead);
+	});
+}
+
+std::size_t
+Transcoder::readSome(std::byte* buffer, std::size_t bufferSize)
+{
+	assert(_childProcess);
+
+	return _childProcess->readSome(buffer, bufferSize);
+}
+
+bool
+Transcoder::finished() const
+{
+	return _childProcess->finished();
 }
 
 } // namespace Transcode

src/libs/av/impl/Transcoder.hpp
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2015 Emeric Poupon
+ *
+ * This file is part of LMS.
+ *
+ * LMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LMS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <filesystem>
+#include <functional>
+
+#include "av/TranscodeParameters.hpp"
+#include "av/Types.hpp"
+
+class IChildProcess;
+
+namespace Av
+{
+	class Transcoder
+	{
+		public:
+			Transcoder(const std::filesystem::path& file, const TranscodeParameters& parameters);
+			~Transcoder();
+
+			Transcoder(const Transcoder&) = delete;
+			Transcoder& operator=(const Transcoder&) = delete;
+			Transcoder(Transcoder&&) = delete;
+			Transcoder& operator=(Transcoder&&) = delete;
+
+			bool			start();
+
+			using WaitCallback = std::function<void()>;
+			void			asyncWaitForData(WaitCallback cb);
+
+			// non blocking calls
+			using ReadCallback = std::function<void(std::size_t nbReadBytes)>;
+			void			asyncRead(std::byte* buffer, std::size_t bufferSize, ReadCallback);
+			std::size_t		readSome(std::byte* buffer, std::size_t bufferSize);
+
+			const std::string&	getOutputMimeType() const { return _outputMimeType; }
+			const TranscodeParameters& getParameters() const { return _parameters; }
+
+			bool			finished() const;
+
+		private:
+			static void init();
+
+			const std::size_t		_id {};
+			const std::filesystem::path	_filePath;
+			const TranscodeParameters	_parameters;
+
+			std::unique_ptr<IChildProcess>	_childProcess;
+
+			bool			_finished {};
+			std::string		_outputMimeType;
+	};
+
+} // namespace Av
+

src/libs/subsonic/impl/Stream.cpp
@@ -19,9 +19,9 @@
 
 #include "Stream.hpp"
 
-#include "av/AvTranscoder.hpp"
-#include "av/AvTranscodeResourceHandlerCreator.hpp"
-#include "av/AvTypes.hpp"
+#include "av/TranscodeParameters.hpp"
+#include "av/TranscodeResourceHandlerCreator.hpp"
+#include "av/Types.hpp"
 #include "database/Session.hpp"
 #include "database/Track.hpp"
 #include "database/User.hpp"
@@ -113,7 +113,7 @@ handleDownload(RequestContext& context, const Wt::Http::Request& request, Wt::Ht
 {
 	std::shared_ptr<IResourceHandler> resourceHandler;
 
-	Wt::Http::ResponseContinuation *continuation = request.continuation();
+	Wt::Http::ResponseContinuation* continuation {request.continuation()};
 	if (!continuation)
 	{
 		// Mandatory params
@@ -137,12 +137,9 @@ handleDownload(RequestContext& context, const Wt::Http::Request& request, Wt::Ht
 		resourceHandler = Wt::cpp17::any_cast<std::shared_ptr<IResourceHandler>>(continuation->data());
 	}
 
-	resourceHandler->processRequest(request, response);
-	if (!resourceHandler->isFinished())
-	{
-		Wt::Http::ResponseContinuation *continuation = response.createContinuation();
+	continuation = resourceHandler->processRequest(request, response);
+	if (continuation)
 		continuation->setData(resourceHandler);
-	}
 }
 
 void
@@ -150,7 +147,7 @@ handleStream(RequestContext& context, const Wt::Http::Request& request, Wt::Http
 {
 	std::shared_ptr<IResourceHandler> resourceHandler;
 
-	Wt::Http::ResponseContinuation *continuation = request.continuation();
+	Wt::Http::ResponseContinuation* continuation = request.continuation();
 	if (!continuation)
 	{
 		StreamParameters streamParameters {getStreamParameters(context)};
@@ -164,12 +161,9 @@ handleStream(RequestContext& context, const Wt::Http::Request& request, Wt::Http
 		resourceHandler = Wt::cpp17::any_cast<std::shared_ptr<IResourceHandler>>(continuation->data());
 	}
 
-	resourceHandler->processRequest(request, response);
-	if (!resourceHandler->isFinished())
-	{
-		Wt::Http::ResponseContinuation *continuation = response.createContinuation();
+	continuation = resourceHandler->processRequest(request, response);
+	if (continuation)
 		continuation->setData(resourceHandler);
-	}
 }
 
 }

src/lms/ui/resource/AudioTranscodeResource.cpp
@@ -19,9 +19,12 @@
 
 #include "AudioTranscodeResource.hpp"
 
+#include <optional>
 #include <Wt/Http/Response.h>
 
-#include "av/AvTranscoder.hpp"
+#include "av/TranscodeParameters.hpp"
+#include "av/TranscodeResourceHandlerCreator.hpp"
+#include "av/Types.hpp"
 #include "database/Session.hpp"
 #include "database/Track.hpp"
 #include "database/User.hpp"
@@ -66,20 +69,13 @@ namespace StringUtils
 	}
 }
 
-namespace UserInterface {
-
-AudioTranscodeResource:: ~AudioTranscodeResource()
-{
-	beingDeleted();
-}
-
 static
 std::optional<Av::Format>
 AudioFormatToAvFormat(Database::AudioFormat format)
 {
 	switch (format)
 	{
-		case Database::AudioFormat::MP3:		return Av::Format::MP3;
+		case Database::AudioFormat::MP3:			return Av::Format::MP3;
 		case Database::AudioFormat::OGG_OPUS:		return Av::Format::OGG_OPUS;
 		case Database::AudioFormat::MATROSKA_OPUS:	return Av::Format::MATROSKA_OPUS;
 		case Database::AudioFormat::OGG_VORBIS:		return Av::Format::OGG_VORBIS;
@@ -89,6 +85,14 @@ AudioFormatToAvFormat(Database::AudioFormat format)
 	LOG(ERROR) << "Cannot convert from audio format to AV format";
 
 	return std::nullopt;
+}
+
+
+namespace UserInterface {
+
+AudioTranscodeResource:: ~AudioTranscodeResource()
+{
+	beingDeleted();
 }
 
 std::string
@@ -115,102 +119,85 @@ readParameterAs(const Wt::Http::Request& request, const std::string& parameterNa
 	return res;
 }
 
+struct TranscodeParameters
+{
+	std::filesystem::path file;
+	Av::TranscodeParameters transcodeParameters;
+};
+
+static
+std::optional<TranscodeParameters>
+readTranscodeParameters(const Wt::Http::Request& request)
+{
+	TranscodeParameters parameters;
+
+	// mandatory parameters
+	auto trackId {readParameterAs<Database::IdType>(request, "trackid")};
+	auto format {readParameterAs<Database::AudioFormat>(request, "format")};
+	auto bitrate {readParameterAs<Database::Bitrate>(request, "bitrate")};
+
+	if (!trackId || !format || !bitrate)
+		return std::nullopt;
+
+	const std::optional<Av::Format> avFormat {AudioFormatToAvFormat(*format)};
+	if (!avFormat)
+		return std::nullopt;
+
+	// optional parameter
+	std::size_t offset {readParameterAs<std::size_t>(request, "offset").value_or(0)};
+
+	std::filesystem::path trackPath;
+	{
+		auto transaction {LmsApp->getDbSession().createSharedTransaction()};
+
+		const Database::Track::pointer track {Database::Track::getById(LmsApp->getDbSession(), *trackId)};
+		if (!track)
+		{
+			LOG(ERROR) << "Missing track";
+			return std::nullopt;
+		}
+
+		parameters.file = track->getPath();
+
+		if (Database::User::audioTranscodeAllowedBitrates.find(*bitrate) == std::cend(Database::User::audioTranscodeAllowedBitrates))
+		{
+			LOG(ERROR) << "Bitrate '" << *bitrate << "' is not allowed";
+			return std::nullopt;
+		}
+	}
+
+	parameters.transcodeParameters.stripMetadata = true;
+	parameters.transcodeParameters.format = *avFormat;
+	parameters.transcodeParameters.bitrate = *bitrate;
+	parameters.transcodeParameters.offset = std::chrono::seconds {offset};
+
+	return parameters;
+}
+
 void
 AudioTranscodeResource::handleRequest(const Wt::Http::Request& request,
 		Wt::Http::Response& response)
 {
-	std::shared_ptr<Av::Transcoder> transcoder;
+	std::shared_ptr<IResourceHandler> resourceHandler;
 
-	// First, see if this request is for a continuation
-	Wt::Http::ResponseContinuation *continuation = request.continuation();
-	if (continuation)
+	Wt::Http::ResponseContinuation* continuation {request.continuation()};
+	if (!continuation)
 	{
-		LOG(DEBUG) << "Continuation! " << continuation ;
-		transcoder = Wt::cpp17::any_cast<std::shared_ptr<Av::Transcoder>>(continuation->data());
+		const std::optional<TranscodeParameters>& parameters {readTranscodeParameters(request)};
+		if (parameters)
+			resourceHandler = Av::createTranscodeResourceHandler(parameters->file, parameters->transcodeParameters);
 	}
 	else
 	{
-		LOG(DEBUG) << "First request: creating transcoder";
-
-		// mandatory parameters
-		auto trackId {readParameterAs<Database::IdType>(request, "trackid")};
-		auto format {readParameterAs<Database::AudioFormat>(request, "format")};
-		auto bitrate {readParameterAs<Database::Bitrate>(request, "bitrate")};
-
-		if (!trackId || !format || !bitrate)
-			return;
-
-		auto avFormat {AudioFormatToAvFormat(*format)};
-		if (!avFormat)
-			return;
-
-		// optional parameter
-		auto offset {readParameterAs<std::size_t>(request, "offset")};
-
-		std::filesystem::path trackPath;
-		{
-			auto transaction {LmsApp->getDbSession().createSharedTransaction()};
-
-			const Database::Track::pointer track {Database::Track::getById(LmsApp->getDbSession(), *trackId)};
-			if (!track)
-			{
-				LOG(ERROR) << "Missing track";
-				return;
-			}
-
-			trackPath = track->getPath();
-
-			if (Database::User::audioTranscodeAllowedBitrates.find(*bitrate) == std::cend(Database::User::audioTranscodeAllowedBitrates))
-			{
-				LOG(ERROR) << "Bitrate '" << *bitrate << "' is not allowed";
-				return;
-			}
-		}
-
-		Av::TranscodeParameters parameters {};
-		parameters.stripMetadata = true;
-		parameters.format = *avFormat;
-		parameters.bitrate = *bitrate;
-		parameters.offset = std::chrono::seconds {offset ? *offset : 0};
-
-		transcoder = std::make_shared<Av::Transcoder>(trackPath, parameters);
-		if (!transcoder->start())
-		{
-			LOG(ERROR) << "Cannot start transcoder";
-			return;
-		}
-
-		LOG(DEBUG) << "Transcoder started";
-
-		std::string mimeType {transcoder->getOutputMimeType()};
-		response.setMimeType(mimeType);
-		LOG(DEBUG) << "Mime type set to '" << mimeType << "'";
-
+		resourceHandler = Wt::cpp17::any_cast<std::shared_ptr<IResourceHandler>>(continuation->data());
 	}
 
-	if (!transcoder->isComplete())
+	if (resourceHandler)
 	{
-		std::vector<unsigned char> data;
-		data.reserve(_chunkSize);
-
-		transcoder->process(data, _chunkSize);
-
-		response.out().write(reinterpret_cast<char*>(&data[0]), data.size());
-		LOG(DEBUG) << "Written " << data.size() << " bytes! complete = " << (transcoder->isComplete() ? "true" : "false");
-
-		if (!response.out())
-		{
-			LOG(ERROR) << "Write failed!";
-		}
+		continuation = resourceHandler->processRequest(request, response);
+		if (continuation)
+			continuation->setData(resourceHandler);
 	}
-
-	if (!transcoder->isComplete() && response.out())
-	{
-		continuation = response.createContinuation();
-		continuation->setData(transcoder);
-	}
-	else
-		LOG(DEBUG) << "No more data!";
 }
 
 } // namespace UserInterface

src/libs/auth/impl/PasswordService.cpp
@@ -82,7 +82,7 @@ checkUserPassword(Database::Session& session, const std::string& loginName, cons
 		case Database::User::AuthMode::Internal:
 		{
 			LMS_LOG(AUTH, DEBUG) << "Checking internal password for user '" << loginName << "'";
-			const Wt::Auth::BCryptHashFunction hashFunc {6}; // TODO parametrize this
+			const Wt::Auth::BCryptHashFunction hashFunc {7}; // TODO parametrize this
 			return hashFunc.verify(password, passwordHash.salt, passwordHash.hash);
 		}
 

src/libs/av/CMakeLists.txt
@@ -1,9 +1,9 @@
 
 add_library(lmsav SHARED
-	impl/AvInfo.cpp
-	impl/AvTranscoder.cpp
-	impl/AvTranscodeResourceHandler.cpp
-	impl/AvTypes.cpp
+	impl/AudioFile.cpp
+	impl/Transcoder.cpp
+	impl/TranscodeResourceHandler.cpp
+	impl/Types.cpp
 	)
 
 target_include_directories(lmsav INTERFACE

commit 05f0213a9fb6fd374058c776e4547ff7ea9afa28

Do not crash when transcoding a non existing file (regression from eea27b8), fixes #141

src/libs/av/impl/TranscodeResourceHandler.cpp
@@ -33,7 +33,6 @@ namespace Av
 	TranscodeResourceHandler::TranscodeResourceHandler(const std::filesystem::path& trackPath, const TranscodeParameters& parameters)
 		: _transcoder {trackPath, parameters}
 	{
-		_transcoder.start();
 	}
 
 	Wt::Http::ResponseContinuation*

src/libs/av/impl/Transcoder.cpp
@@ -48,11 +48,12 @@ Transcoder::Transcoder(const std::filesystem::path& filePath, const TranscodePar
 , _filePath {filePath}
 , _parameters {parameters}
 {
+	start();
 }
 
 Transcoder::~Transcoder() = default;
 
-bool
+void
 Transcoder::start()
 {
 	if (ffmpegPath.empty())
@@ -61,20 +62,13 @@ Transcoder::start()
 	try
 	{
 		if (!std::filesystem::exists(_filePath))
-		{
-			LOG(ERROR) << "File '" << _filePath << "' does not exist!";
-			return false;
-		}
+			throw Exception {"File '" + _filePath.string() + "' does not exist!"};
 		else if (!std::filesystem::is_regular_file( _filePath) )
-		{
-			LOG(ERROR) << "File '" << _filePath << "' is not regular!";
-			return false;
-		}
+			throw Exception {"File '" + _filePath.string() + "' is not regular!"};
 	}
 	catch (const std::filesystem::filesystem_error& e)
 	{
-		LOG(ERROR) << "File error on '" << _filePath.string() << "': " << e.what();
-		return false;
+		throw Exception {"File error '" + _filePath.string() + "': " + e.what()};
 	}
 
 	LOG(INFO) << "Transcoding file '" << _filePath.string() << "'";
@@ -163,7 +157,7 @@ Transcoder::start()
 			break;
 
 		default:
-			return false;
+			throw Exception {"Unhandled format (" + std::to_string(static_cast<int>(_parameters.format)) + ")"};
 	}
 
 	_outputMimeType = formatToMimetype(_parameters.format);
@@ -181,11 +175,8 @@ Transcoder::start()
 	}
 	catch (ChildProcessException& exception)
 	{
-		LOG(ERROR) << "Cannot execute '" << ffmpegPath << "': " << exception.what();
-		return false;
+		throw Exception {"Cannot execute '" + ffmpegPath.string() + "': " + exception.what()};
 	}
-
-	return true;
 }
 
 void
@@ -223,6 +214,8 @@ Transcoder::readSome(std::byte* buffer, std::size_t bufferSize)
 bool
 Transcoder::finished() const
 {
+	assert(_childProcess);
+
 	return _childProcess->finished();
 }
 

src/libs/av/impl/Transcoder.hpp
@@ -40,8 +40,6 @@ namespace Av
 			Transcoder(Transcoder&&) = delete;
 			Transcoder& operator=(Transcoder&&) = delete;
 
-			bool			start();
-
 			using WaitCallback = std::function<void()>;
 			void			asyncWaitForData(WaitCallback cb);
 
@@ -57,6 +55,8 @@ namespace Av
 
 		private:
 			static void init();
+
+			void start();
 
 			const std::size_t		_id {};
 			const std::filesystem::path	_filePath;

src/libs/subsonic/impl/Stream.cpp
@@ -26,6 +26,7 @@
 #include "database/Track.hpp"
 #include "database/User.hpp"
 #include "utils/IResourceHandler.hpp"
+#include "utils/Logger.hpp"
 #include "utils/FileResourceHandlerCreator.hpp"
 #include "utils/Utils.hpp"
 #include "ParameterParsing.hpp"
@@ -147,24 +148,30 @@ handleStream(RequestContext& context, const Wt::Http::Request& request, Wt::Http
 {
 	std::shared_ptr<IResourceHandler> resourceHandler;
 
-	Wt::Http::ResponseContinuation* continuation = request.continuation();
-	if (!continuation)
+	try
 	{
-		StreamParameters streamParameters {getStreamParameters(context)};
-		if (streamParameters.transcodeParameters)
-			resourceHandler = Av::createTranscodeResourceHandler(streamParameters.trackPath, *streamParameters.transcodeParameters);
+		Wt::Http::ResponseContinuation* continuation = request.continuation();
+		if (!continuation)
+		{
+			StreamParameters streamParameters {getStreamParameters(context)};
+			if (streamParameters.transcodeParameters)
+				resourceHandler = Av::createTranscodeResourceHandler(streamParameters.trackPath, *streamParameters.transcodeParameters);
+			else
+				resourceHandler = createFileResourceHandler(streamParameters.trackPath);
+		}
 		else
-			resourceHandler = createFileResourceHandler(streamParameters.trackPath);
+		{
+			resourceHandler = Wt::cpp17::any_cast<std::shared_ptr<IResourceHandler>>(continuation->data());
+		}
+
+		continuation = resourceHandler->processRequest(request, response);
+		if (continuation)
+			continuation->setData(resourceHandler);
 	}
-	else
+	catch (const Av::Exception& e)
 	{
-		resourceHandler = Wt::cpp17::any_cast<std::shared_ptr<IResourceHandler>>(continuation->data());
+		LMS_LOG(API_SUBSONIC, ERROR) << "Caught Av exception: " << e.what();
 	}
-
-	continuation = resourceHandler->processRequest(request, response);
-	if (continuation)
-		continuation->setData(resourceHandler);
 }
 
-}
-
+} // namespace API::Subsonic::Stream

src/lms/ui/resource/AudioTranscodeResource.cpp
@@ -180,23 +180,30 @@ AudioTranscodeResource::handleRequest(const Wt::Http::Request& request,
 {
 	std::shared_ptr<IResourceHandler> resourceHandler;
 
-	Wt::Http::ResponseContinuation* continuation {request.continuation()};
-	if (!continuation)
+	try
 	{
-		const std::optional<TranscodeParameters>& parameters {readTranscodeParameters(request)};
-		if (parameters)
-			resourceHandler = Av::createTranscodeResourceHandler(parameters->file, parameters->transcodeParameters);
-	}
-	else
-	{
-		resourceHandler = Wt::cpp17::any_cast<std::shared_ptr<IResourceHandler>>(continuation->data());
-	}
+		Wt::Http::ResponseContinuation* continuation {request.continuation()};
+		if (!continuation)
+		{
+			const std::optional<TranscodeParameters>& parameters {readTranscodeParameters(request)};
+			if (parameters)
+				resourceHandler = Av::createTranscodeResourceHandler(parameters->file, parameters->transcodeParameters);
+		}
+		else
+		{
+			resourceHandler = Wt::cpp17::any_cast<std::shared_ptr<IResourceHandler>>(continuation->data());
+		}
 
-	if (resourceHandler)
+		if (resourceHandler)
+		{
+			continuation = resourceHandler->processRequest(request, response);
+			if (continuation)
+				continuation->setData(resourceHandler);
+		}
+	}
+	catch (const Av::Exception& e)
 	{
-		continuation = resourceHandler->processRequest(request, response);
-		if (continuation)
-			continuation->setData(resourceHandler);
+		LOG(ERROR) << "Caught Av exception: " << e.what();
 	}
 }
 