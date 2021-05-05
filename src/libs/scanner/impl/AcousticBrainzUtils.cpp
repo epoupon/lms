d9df72d9cf9bec047a8b510cd493afd7488ccc62
emeric <emeric.poupon@laposte.net>
2021-05-05 12:31:56 +0200

    Restored AcousticBrainz

--- src/libs/scanner/impl/AcousticBrainzUtils.cpp
+++ src/libs/scanner/impl/AcousticBrainzUtils.cpp
@@ -38,9 +38,9 @@ static
 std::string
 getJsonData(const UUID& mbid)
 {
-	static constexpr std::string_view defaultAPIURL {"https://acousticbrainz.org/api"};
+	static constexpr std::string_view defaultAPIURL {"https://acousticbrainz.org"};
 
-	const std::string url {std::string {Service<IConfig>::get()->getString("acousticbrainz-api-base-url", defaultAPIURL)} + std::string {mbid.getAsString()} + "/low-level"};
+	const std::string url {std::string {Service<IConfig>::get()->getString("acousticbrainz-api-base-url", defaultAPIURL)} + "/api/v1/" + std::string {mbid.getAsString()} + "/low-level"};
 
 	boost::asio::io_service ioService;
 
