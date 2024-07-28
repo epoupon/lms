cb7ee34d417ba0ef8c5b0cb08f238578bfa30b1d
emeric <emeric.poupon@laposte.net>
2024-07-28 14:52:37 +0200

    Added a missing index to speed up direcotry browsing commands

--- src/libs/database/impl/Session.cpp
+++ src/libs/database/impl/Session.cpp
@@ -206,6 +206,7 @@ namespace lms::db
 
         _session.execute("CREATE INDEX IF NOT EXISTS track_id_idx ON track(id)");
         _session.execute("CREATE INDEX IF NOT EXISTS track_absolute_path_idx ON track(absolute_file_path)");
+        _session.execute("CREATE INDEX IF NOT EXISTS track_directory_release_idx ON track(directory_id, release_id);");
         _session.execute("CREATE INDEX IF NOT EXISTS track_name_idx ON track(name)");
         _session.execute("CREATE INDEX IF NOT EXISTS track_name_nocase_idx ON track(name COLLATE NOCASE)");
         _session.execute("CREATE INDEX IF NOT EXISTS track_mbid_idx ON track(mbid)");
