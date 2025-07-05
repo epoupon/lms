41504c92e0c97c557d3d3cfc07c05220093e7482
emeric <emeric.poupon@laposte.net>
2025-07-05 10:25:29 +0200

    Fixed unity build

--- src/libs/services/scanner/impl/steps/JobQueue.hpp
+++ src/libs/services/scanner/impl/steps/JobQueue.hpp
@@ -16,6 +16,9 @@
  * You should have received a copy of the GNU General Public License
  * along with LMS.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#pragma once
+
 #include <functional>
 #include <memory>
 #include <span>
